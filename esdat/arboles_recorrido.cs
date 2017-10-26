917a88019eaa0ead61f8f57015958283d3ed0934
tic-rt16311016@uthermosillo.edu.mx <beatrizkrn@hotmail.com>
2017-10-25 23:01:27 -0700

    ...

--- esdat/arboles_recorrido.cs
+++ esdat/arboles_recorrido.cs
@@ -89,6 +89,7 @@ namespace esdat
                     item.Remove();
                     Lista.Remove(item.Text);
                     break;
+
                 }
                 else
                 {
